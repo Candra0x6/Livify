014c02d0077d7e3c76f319e987f2dc773da95ea3
C N <147255377+Candra0x6@users.noreply.github.com>
2024-07-31 11:13:56 +0700

    Make SectionHeader component responsive for better user experience on all screen sizes

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Description } from "@radix-ui/react-dialog";
+import { useRouter } from "next/navigation";
 import type { ReactNode } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 
@@ -8,25 +9,34 @@ type props = {
   title: string;
   description: string;
   navigate?: string | null;
+  href?: string | null;
 };
 export default function SectionHeader({
   title,
   description,
   navigate,
 }: props): ReactNode {
-  console.log(navigate);
+  const router = useRouter();
   return (
     <div className="flex justify-between items-end mb-5">
       <div className="">
-        <h1 className="text-[2.5rem] font-bold">{title}</h1>
-        <span className="text-[#A5A3A3]">{description}</span>
+        <h1 className="xl:text-[2.5rem] lg:text-[2.2rem] sm:text-[2rem] text-[1.5rem] font-bold">
+          {title}
+        </h1>
+        <span className="text-textSecondary text-[0.8rem] sm:text-[1rem] lg:text-[1.2rem]">
+          {description}
+        </span>
       </div>
       {navigate === undefined ? (
         ""
       ) : (
-        <div className="flex items-center h-full gap-x-2 text-textSecondary group hover:translate-x-2 transition-all duration-300 ease-in-out">
+        <div
+          onClick={() => router.push("/products")}
+          onKeyUp={() => router.push("/products")}
+          className=" items-center h-full gap-x-2 text-textSecondary group md:flex hidden cursor-pointer"
+        >
           <span className="">{navigate}</span>
-          <FaArrowRightLong className="" />
+          <FaArrowRightLong className="group-hover:translate-x-2 transition-all duration-300 ease-in-out " />
         </div>
       )}
     </div>
