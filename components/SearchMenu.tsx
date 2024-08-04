0bb53e49c69453e9d51e9794a799f92d9920002e
C N <147255377+Candra0x6@users.noreply.github.com>
2024-08-04 17:59:58 +0700

    feat: add search component

---
+++ components/SearchMenu.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import * as ScrollArea from "@radix-ui/react-scroll-area";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import { AppWindowMac } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { BiWindows } from "react-icons/bi";
+import { CiSearch } from "react-icons/ci";
+import { IoLogoWindows } from "react-icons/io5";
+import { MdOutlineDesktopWindows } from "react-icons/md";
+import { PiWindowsLogo, PiWindowsLogoFill } from "react-icons/pi";
+import { TbLocationSearch } from "react-icons/tb";
+import { Button } from "./ui/button";
+
+export default function SearchModal() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "/" && !isOpen) {
+        event.preventDefault();
+        setIsOpen(true);
+      }
+      if (event.key === "k" && (event.metaKey || event.ctrlKey)) {
+        event.preventDefault();
+        setIsOpen((open) => !open);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen]);
+
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
+      <div className="flex lg:w-auto w-full">
+        <Dialog.Trigger asChild>
+          <div className="lg:w-[230px] w-full flex bg-accent hover:bg-accent/80 items-center rounded-md h-10 ml-2 cursor-pointer">
+            <div className="p-4 flex items-center lg:justify-between w-full">
+              <Button variant="ghost" className="rounded-none text-white p-0">
+                <CiSearch className="text-2xl text-foreground" />
+              </Button>
+              <div className="w-full flex justify-between items-center">
+                <h1 className="lg:text-sm text-xs ml-2"> Search</h1>
+                <span className="text-foreground/70 text-sm font-black lg:flex hidden">
+                  Ctrl K
+                </span>
+              </div>
+            </div>
+          </div>
+        </Dialog.Trigger>
+      </div>
+      <Dialog.Portal>
+        <Dialog.Overlay className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50" />
+        <Dialog.Content className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid max-h-[85vh] w-[90vw] max-w-lg translate-x-[-50%] translate-y-[-50%] overflow-hidden border bg-white shadow-lg duration-200 sm:rounded-lg">
+          <VisuallyHidden.Root>
+            <Dialog.Title>Search</Dialog.Title>
+            <Dialog.Description>
+              Start typing to search the documentation
+            </Dialog.Description>
+          </VisuallyHidden.Root>
+          <form className="border-b border-slate-200">
+            <div className="flex items-center">
+              <VisuallyHidden.Root>
+                <label htmlFor="search-modal">Search</label>
+              </VisuallyHidden.Root>
+              <CiSearch className="ml-4 h-5 w-5 shrink-0 text-slate-500" />
+
+              <input
+                id="search-modal"
+                className="[&::-webkit-search-decoration]:none [&::-webkit-search-results-button]:none [&::-webkit-search-results-decoration]:none [&::-webkit-search-cancel-button]:hidden w-full appearance-none border-0 bg-white py-3 pl-2 pr-4 text-sm placeholder-slate-400 text-black focus:outline-none"
+                type="search"
+                placeholder="Search"
+              />
+            </div>
+          </form>
+          <ScrollArea.Root className="max-h-[calc(85vh-44px)]">
+            <ScrollArea.Viewport className="h-full w-full">
+              <div className="space-y-4 px-2 py-4">
+                <div>
+                  <div className="mb-2 px-2 text-xs font-semibold uppercase text-gray-400">
+                    Suggestions
+                  </div>
+                  <ul>
+                    <li>
+                      <Link
+                        className="flex items-center rounded-lg px-2 py-1 text-sm leading-6 text-slate-700 outline-none focus-within:bg-slate-100 hover:bg-slate-100"
+                        href="#0"
+                      >
+                        <TbLocationSearch className="mr-3 h-4 w-4 shrink-0 fill-slate-400 text-slate-400" />
+
+                        <span>Flexbox and Grid</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </ScrollArea.Viewport>
+            <ScrollArea.Scrollbar
+              className="flex h-full w-2 touch-none select-none border-l border-l-transparent p-[1px] transition-colors"
+              orientation="vertical"
+            >
+              <ScrollArea.Thumb className="relative flex-1 rounded-full bg-slate-300" />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Scrollbar
+              className="flex h-2.5 touch-none select-none flex-col border-t border-t-transparent p-[1px] transition-colors"
+              orientation="horizontal"
+            >
+              <ScrollArea.Thumb className="relative flex-1 rounded-full bg-slate-300" />
+            </ScrollArea.Scrollbar>
+            <ScrollArea.Corner className="bg-blackA5" />
+          </ScrollArea.Root>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
