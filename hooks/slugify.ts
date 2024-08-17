1f734eb0904057db9a3efea7ac867ee5dfe7c83e
C N <147255377+Candra0x6@users.noreply.github.com>
2024-08-17 12:44:39 +0700

    refactor(slugify): extract function to custom hook

--- hooks/slugify.ts
+++ hooks/slugify.ts
@@ -1,6 +1,6 @@
 function slugify(text: string): string {
-	const convert: string = text.toLowerCase().replace(/ /g, "-");
-	return convert;
+  const convert: string = text?.toLowerCase().replace(/ /g, "-");
+  return convert;
 }
 
 export default slugify;
