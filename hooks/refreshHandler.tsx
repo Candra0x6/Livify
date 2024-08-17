a0d307ea13a5d38c523fdd913dd4ca78c8e42008
C N <147255377+Candra0x6@users.noreply.github.com>
2024-08-17 12:37:09 +0700

    remove : unuse console.log

--- hooks/refreshHandler.tsx
+++ hooks/refreshHandler.tsx
@@ -5,48 +5,49 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export interface sessionEror extends Session {
-	message?: string;
+  message?: string;
 }
 interface propsType {
-	initialSession: sessionEror;
-	refreshToken: RefreshSession;
+  initialSession: sessionEror;
+  refreshToken: RefreshSession;
 }
 export default function RefreshHandler({
-	initialSession,
-	refreshToken,
+  initialSession,
+  refreshToken,
 }: propsType) {
-	const [session, setSession] = useState(initialSession);
-	const router = useRouter();
-	useEffect(() => {
-		if (session.message === "Expire") {
-			const refreshSession = async () => {
-				try {
-					const response = await fetch(
-						`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/session/refresh`,
-						{
-							method: "PATCH",
-							headers: {
-								"Content-Type": "application/json",
-							},
-							body: JSON.stringify(refreshToken),
-						},
-					);
+  console.log(initialSession);
+  const [session, setSession] = useState(initialSession);
+  const router = useRouter();
+  useEffect(() => {
+    if (session.message === "Expire") {
+      const refreshSession = async () => {
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/session/refresh`,
+            {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(refreshToken),
+            }
+          );
 
-					if (response.ok) {
-						const newSession = await response.json();
-						setSession(newSession);
-						router.refresh(); // Memicu re-render komponen server
-					} else {
-						router.push("/sign-in");
-					}
-				} catch (error) {
-					console.error("Failed to refresh session:", error);
-				}
-			};
+          if (response.ok) {
+            const newSession = await response.json();
+            setSession(newSession);
+            router.refresh();
+          } else {
+            router.push("/sign-in");
+          }
+        } catch (error) {
+          console.error("Failed to refresh session:", error);
+        }
+      };
 
-			refreshSession();
-		}
-	}, [session, refreshToken, router]);
+      refreshSession();
+    }
+  }, [session, refreshToken, router]);
 
-	return null; // Komponen ini tidak me-render apapun
+  return null;
 }
