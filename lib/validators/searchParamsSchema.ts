a3ad21822348e41a81264131570af451655bee5e
C N <147255377+Candra0x6@users.noreply.github.com>
2024-08-17 12:47:52 +0700

    feat(schema) : add order query schema

--- lib/validators/searchParamsSchema.ts
+++ lib/validators/searchParamsSchema.ts
@@ -1,34 +1,54 @@
+import { ORDER_STATUS } from "@prisma/client";
 import { z } from "zod";
+import { ordersQuerySchema } from "./orderSchema";
 
 export const searchQuerySchema = z.object({
-	query: z.string(),
-	limit: z
-		.string()
-		.optional()
-		.transform((val) => (val ? Number.parseInt(val, 10) : 10)),
-	page: z
-		.string()
-		.optional()
-		.transform((val) => (val ? Number.parseInt(val, 10) : 1)),
-	sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
+  query: z.string(),
+  format: z.enum(["simple", "detailed"]).optional().default("simple"),
+  limit: z
+    .string()
+    .optional()
+    .transform((val) => (val ? Number.parseInt(val, 10) : 10)),
+  page: z
+    .string()
+    .optional()
+    .transform((val) => (val ? Number.parseInt(val, 10) : 1)),
+  sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
 });
 
 export const productQuerySchema = z.object({
-	limit: z
-		.string()
-		.optional()
-		.transform((val) => (val ? Number.parseInt(val, 10) : 10)),
-	page: z
-		.string()
-		.optional()
-		.transform((val) => (val ? Number.parseInt(val, 10) : 1)),
-	categoryId: z.string().optional(),
-	sortBy: z
-		.enum(["name", "price", "createdAt"])
-		.optional()
-		.default("createdAt"),
-	sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
+  limit: z
+    .string()
+    .optional()
+    .transform((val) => (val ? Number.parseInt(val, 10) : 10)),
+  page: z
+    .string()
+    .optional()
+    .transform((val) => (val ? Number.parseInt(val, 10) : 1)),
+  categoryId: z.string().optional(),
+  sortBy: z
+    .enum(["name", "price", "createdAt"])
+    .optional()
+    .default("createdAt"),
+  sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
 });
 
+export const orderQuerySchema = z.object({
+  query: z.string(),
+  status: z
+    .enum(["COMPLETED", "REJECTED", "PROCESSING", "PENDING", "CANCELLED"])
+    .optional()
+    .nullable()
+    .catch(null),
+  limit: z
+    .string()
+    .optional()
+    .transform((val) => (val ? Number.parseInt(val, 10) : 10)),
+  page: z
+    .string()
+    .optional()
+    .transform((val) => (val ? Number.parseInt(val, 10) : 1)),
+  sortOrder: z.enum(["asc", "desc"]).optional().default("desc"),
+});
 export type searchQueryPayload = z.infer<typeof searchQuerySchema>;
 export type productQueryPayload = z.infer<typeof productQuerySchema>;
