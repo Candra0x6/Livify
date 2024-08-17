44c38431c9c4031823d2acd533b494ceea85037d
C N <147255377+Candra0x6@users.noreply.github.com>
2024-08-17 12:50:43 +0700

    react toast library & rechart

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 	reactStrictMode: true,
+	images: {
+		domains: ["vqeowfcbngeyipwzpkul.supabase.co"],
+	},
 };
 
 module.exports = nextConfig;
