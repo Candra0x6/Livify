f4c1b3b4aca75fb9572fef97ab070335e306e9d3
C N <147255377+Candra0x6@users.noreply.github.com>
2024-08-17 12:44:53 +0700

    refactor(session): extract function to custom hook

---
+++ hooks/auth/useSession.ts
@@ -0,0 +1,20 @@
+export const useSession = () => {
+
+
+  const newSession = async (userId: string) => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/session/new`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId }),
+      }
+    );
+    const data = await response.json()
+    return response
+
+  }
+  return { newSession }
+}
