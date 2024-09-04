939ead8c5ea0b71f6418c9bfccfce9e53a51a066
C N <147255377+Candra0x6@users.noreply.github.com>
2024-09-04 16:06:12 +0700

    remove : unused console

--- app/api/v1/payment/charge/route.ts
+++ app/api/v1/payment/charge/route.ts
@@ -6,7 +6,6 @@ import type { NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json()
-    console.log(body)
     const amount = Math.round(body.amount * 100)
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
